66a5dfffa9c253a9489e4b04d567b32f6ec37263
pratik vakil <vakilpratik@gmail.com>
2017-04-27 20:26:47 +0530

    Updated for wasm

--- webassembly/2.c
+++ webassembly/2.c
@@ -1,6 +1,8 @@
 
-// emcc 2.c -s WASM=1 -s ONLY_MY_CODE=1 -s EXPORTED_FUNCTIONS=['_foo'] -s STRICT=1 -o 2.html
+// emcc 2.c -s WASM=1 -s ONLY_MY_CODE=1 -s EXPORTED_FUNCTIONS="['_foo','_foo1']" -s STRICT=1 -o 2.html
 
 int foo(){
 	return 101;
 }
+
+float foo1( float a, float b) { return a + b; }
